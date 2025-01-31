2a522b059009eea434a341e93060191b040eb267
enzochristo <enzosc1@al.insper.edu.br>
2025-01-31 00:54:28 -0300

    fit: adding plan model

--- src/models/plan_model.py
+++ src/models/plan_model.py
@@ -1,28 +1,22 @@
 from mongoengine import *
-import datetime
-from models.fields.sensivity_field import SensivityField
 import os
 import dotenv
-import bcrypt
 from cryptography.fernet import Fernet
 
 dotenv.load_dotenv()
 fernet = Fernet(os.getenv("FERNET_SECRET_KEY"))
 
-class PlansModel(Document):
-    sensivity_fields = [
-        
-    ]
-
+class PlanModel(Document):
+    sensivity_fields = []
+    
     name = StringField(required=True)
-    customer_id = StringField(required=True)
-    tipo = StringField(required=True)
-    price = FloatField(required = True)
+    tipo = StringField(required=True, choices=["Internet", "Mobile", "Fixed"])
+    price = FloatField(required=True)
     benefits = StringField()
-    created_at: DateTimeField(default=datetime.utcnow)
-    updated_at: DateTimeField()
-
+    vel_max = IntField(required=False)
+    vel_min = IntField(required=False)
+    created_at = DateTimeField()
+    updated_at = DateTimeField()
 
     def get_normal_fields():
-        return [i for i in PlansModel.__dict__.keys() if i[:1] != '_' and i != "sensivity_fields" and i not in PlanModel.sensivity_fields]
-    
+        return [i for i in PlanModel.__dict__.keys() if i[:1] != '_' and i != "sensivity_fields"]
